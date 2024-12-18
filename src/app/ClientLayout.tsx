c1c4923eaf7d21e349ac29a33e8752bd3f555515
Eric Venturino <eric.venturino@orange.fr>
2024-12-18 23:52:15 +0100

    resolution metadata & useserver

---
+++ src/app/ClientLayout.tsx
@@ -0,0 +1,18 @@
+// src/app/ClientLayout.tsx
+"use client";
+
+import { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  const [dark, setDark] = useState(true);
+
+  return (
+    <>
+      <Navbar dark={dark} toggleDarkMode={() => setDark(!dark)} />
+      {children}
+      <Footer />
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// page.tsx
+import ClientLayout from "./ClientLayout";
+import style from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
   return (
-
-      <main className={styles.main}>
-        </main>
-       
-
+    <ClientLayout>
+      <main className={style.main}>
+        <h1>Bienvenue sur la page principale</h1>
+        <p>Ceci est un exemple de contenu.</p>
+      </main>
+    </ClientLayout>
   );
 }
