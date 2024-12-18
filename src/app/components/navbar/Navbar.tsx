d84445aeeed143f88d65d967f9b3e787684d42bc
Eric Venturino <eric.venturino@orange.fr>
2024-12-18 23:33:47 +0100

    integration dark mode toogle & css

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -2,8 +2,15 @@
 
 import { useState, useEffect } from "react";
 import style from "./Navbar.module.css"; // Importation correcte du CSS module
+import Image from "next/image";
+import logo from "../../../../public/react_logo.svg";
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+  dark: boolean;
+  toggleDarkMode: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ dark, toggleDarkMode }) => {
   const [scroll, setScroll] = useState(false);
 
   useEffect(() => {
@@ -26,10 +33,18 @@ const Navbar: React.FC = () => {
     >
       <ul className={style.navbarUl}>
         <li className={style.navbarLi}>
-          <span className={style.navbarSpan}>⚛️</span> {"<"}Framework{">"}
+          <Image src={logo} alt="logo react" width={20} height={20} />
+          React
         </li>
         <li className={style.navbarLi}>News</li>
         <li className={style.navbarLi}>Contact</li>
+        <li className={style.toogle} onClick={toggleDarkMode}>
+          <input
+            type="button"
+            value={""}
+            className={`${style.mode} ${dark ? style.ligth : style.dark}`}
+          ></input>
+        </li>
       </ul>
     </nav>
   );
